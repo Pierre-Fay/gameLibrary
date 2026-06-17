266d50f8dd6a142b7819a9397061d92dda81d30e
Pierre <Pierre-Fay@users.noreply.github.com>
2026-06-17 13:50:25 +0200

    S1002 added customer bo

---
+++ src/main/java/fr/eni/gamelibrary/bo/Customer.java
@@ -0,0 +1,30 @@
+package fr.eni.gamelibrary.bo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+}
